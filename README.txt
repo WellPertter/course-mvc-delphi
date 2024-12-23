5ed505db9acf424eb342605a4415a73a6a489621
WellPertter <josearthur.12311@gmail.com>
2024-12-23 10:18:32 -0300

    Add the interface of connection and the class TConnecntion

--- README.txt
+++ README.txt
@@ -3,3 +3,8 @@ curso MVC
 Módulo   - Regra de negócio
 View     - visual
 Controle - Intermediar as requisições
+
+O 
+TCustomConnection é o mais ancestral do TFDConnection
+Ou seja, se o retorno for TFDConnection, posso retornar um 
+TFDConnection
---
+++ src/model/connection/impl/mvc.model.connection.impl.firedac.pas
@@ -0,0 +1,54 @@
+unit mvc.model.connection.impl.firedac;
+
+interface
+
+uses
+  Data.DB,
+  System.SysUtils,
+  FireDAC.Comp.Client,
+  FireDAC.Comp.DataSet,
+  FireDAC.Comp.UI,
+  mvc.model.connection.interfaces;
+
+type
+  TConnectionFireDac = class(TInterfacedObject, iConnection)
+  private
+    Fconn : TFDConnection;
+    constructor Create;
+    destructor Destroy; override;
+  public
+    class function New: iConnection;
+    function Connection: TCustomConnection;
+  end;
+
+implementation
+
+{ TConnectionFireDac }
+
+function TConnectionFireDac.Connection: TCustomConnection;
+begin
+  Result := Fconn;
+end;
+
+constructor TConnectionFireDac.Create;
+begin
+  Fconn := TFDConnection.Create(nil);
+  try
+
+  except
+    raise Exception.Create('Erro ao realizar a conexão com o banco de dados!');
+  end;
+end;
+
+destructor TConnectionFireDac.Destroy;
+begin
+  Fconn.free;
+  inherited;
+end;
+
+class function TConnectionFireDac.New: iConnection;
+begin
+  Result := TConnectionFireDac.Create;
+end;
+
+end.
